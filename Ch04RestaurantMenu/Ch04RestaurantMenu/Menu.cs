3c4cf91ddb7b2d0d0748d9e562d0f6c0cfd7fb86
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-15 17:02:24 -0400

    fixed comments on List initialization

--- Ch04RestaurantMenu/Ch04RestaurantMenu/Menu.cs
+++ Ch04RestaurantMenu/Ch04RestaurantMenu/Menu.cs
@@ -3,7 +3,7 @@ namespace Ch04RestaurantMenu
     public class Menu
     {
         // FIELDS
-        public List<MenuItem> MenuItems { get; set; } = []; // new ArrayList<>() or new()
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>(); // or new() or []
         public DateTime LastUpdated { get; set; } = DateTime.Now;
 
         // Default constructor is fine
--- Ch05RestaurantMenu/Ch05RestaurantMenu/Menu.cs
+++ Ch05RestaurantMenu/Ch05RestaurantMenu/Menu.cs
@@ -5,7 +5,7 @@ namespace Ch05RestaurantMenu
     public class Menu
     {
         // FIELDS
-        public List<MenuItem> MenuItems { get; set; } = []; // new ArrayList<>() or new()
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>(); // or new() or []
         public DateTime LastUpdated { get; set; } = DateTime.Now;
 
         // Default constructor is fine
