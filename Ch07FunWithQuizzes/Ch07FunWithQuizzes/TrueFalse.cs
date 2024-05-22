bee64d47b4b635dd83a4132787373877d59c84ab
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-22 02:19:36 -0400

    updated TODOs for TrueFalse

--- Ch07FunWithQuizzes/Ch07FunWithQuizzes/TrueFalse.cs
+++ Ch07FunWithQuizzes/Ch07FunWithQuizzes/TrueFalse.cs
@@ -4,6 +4,7 @@
 public class TrueFalse
 {
     // TODO: Generate a standard constructor that is inherited
+    // Then modify to accept boolean and create True/False choices accordingly
 
     // TODO: Override ToString() to include prompt and formatted choices
 }
