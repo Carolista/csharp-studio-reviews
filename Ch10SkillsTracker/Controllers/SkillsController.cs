e087b2df0b46360be4ba1fe5457243e7af995636
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-29 20:26:26 -0400

    updated to be consistent with newer instructions

--- Ch10SkillsTracker/Controllers/SkillsController.cs
+++ Ch10SkillsTracker/Controllers/SkillsController.cs
@@ -1,25 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ch10SkillsTracker;
 
 [Route("/skills")]
 public class SkillsController : Controller
 {
+    // Print-friendly text
+    Dictionary<string, string> progressOptions = new() {
+        { "learningBasics", "Learning the basics"},
+        { "buildingApps", "Building some apps"},
+        { "masterCoder", "Master coder" }
+    };
+
     // Endpoint: GET /skills
     [HttpGet] // defaults to base route for controller
     public IActionResult RenderSkillsPage()
     {
         string html = 
             "<h1>Skills Tracker</h1>" +
-            "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
+            "<h2>Coding Skills to Learn</h2>" +
             "<ol>" +
             "<li>C#</li>" +
-            "<li>Java</li>" +
             "<li>JavaScript</li>" +
-            "<li>TypeScript</li>" +
             "<li>Ruby</li>" +
             "</ol>" +
-            "<p>Click <a href='/skills/form'>here</a> to select your favorite.</p>";
+            "<p>Click <a href='/skills/form'>here</a> to update your learning progress.</p>";
 
         return Content(html, "text/html");
     }
@@ -28,29 +34,33 @@ public class SkillsController : Controller
     [HttpGet("form")]
     public IActionResult RenderSkillsForm()
     {
-        string options = 
-            "<option value='CSharp'>C#</option>" +
-            "<option value='Java'>Java</option>" +
-            "<option value='JavaScript'>JavaScript</option>" +
-            "<option value='TypeScript'>TypeScript</option>" +
-            "<option value='Ruby'>Ruby</option>";
+        StringBuilder optionsHTML = new();
+        foreach(KeyValuePair<string, string> option in progressOptions)
+        {
+            optionsHTML
+                .Append("<option value='")
+                .Append(option.Key)
+                .Append("'>")
+                .Append(option.Value)
+                .Append("</option>");
+        }
 
         string html = 
             // BONUS MISSION 2 - Post to /results instead of /form
             "<form action='/skills/results' method='POST'>" +
-            "<label>Name:</label>" +
-            "<input type='text' name='userName' /><br />" +
-            "<label>My favorite language:</label>" +
-            "<select name='fave1'>" +
-            options +
+            "<label>Today's Date:</label>" +
+            "<input type='date' name='updated' /><br />" +
+            "<label>C# Progress:</label>" +
+            "<select name='cSharpProgress'>" +
+            optionsHTML +
             "</select><br />" +
-            "<label>My second favorite language:</label>" +
-            "<select name='fave2'>" +
-            options +
+            "<label>JavaScript Progress:</label>" +
+            "<select name='jSProgress'>" +
+            optionsHTML +
             "</select><br />" +
-            "<label>My third favorite language:</label>" +
-            "<select name='fave3'>" +
-            options +
+            "<label>Ruby Progress:</label>" +
+            "<select name='rubyProgress'>" +
+            optionsHTML +
             "</select><br />" +
             "<button type='submit'>Submit</button>" +
             "</form>";
@@ -61,15 +71,15 @@ public class SkillsController : Controller
     // Endpoint: POST /skills/form
     // This is no longer used after bonus mission
     [HttpPost("form")]
-    public IActionResult ProcessSkillsForm(string username, string fave1, string fave2, string fave3)
+    public IActionResult ProcessSkillsForm(string updated, string cSharpProgress, string jSProgress, string rubyProgress)
     {
         string html = 
-            "<h1>" + username + "</h1>" +
-            "<h3>Favorite Languages</h3>" +
+            "<h1>" + updated + "</h1>" +
+            "<h3>My Progress</h3>" +
             "<ol>" +
-            "<li>" + fave1 + "</li>" +
-            "<li>" + fave2 + "</li>" +
-            "<li>" + fave3 + "</li>" +
+            "<li>C#: " + progressOptions[cSharpProgress] + "</li>" +
+            "<li>JavaScript: " + progressOptions[jSProgress] + "</li>" +
+            "<li>Ruby: " + progressOptions[rubyProgress] + "</li>" +
             "</ol>";
 
         return Content(html, "text/html");
@@ -79,23 +89,23 @@ public class SkillsController : Controller
     // BONUS MISSION 1 - Use table instead of ordered list
     // BONUS MISSION 2 - Route to /results instead of /form
     [HttpPost("results")]
-    public IActionResult ProcessSkillsFormBonus(string username, string fave1, string fave2, string fave3)
+    public IActionResult ProcessSkillsFormBonus(string updated, string cSharpProgress, string jSProgress, string rubyProgress)
     {
         string html = 
-            "<h1>" + username + "</h1>" +
-            "<h3>Favorite Languages</h3>" +
+            "<h1>" + updated + "</h1>" +
+            "<h3>My Progress</h3>" +
             "<table>" +
             "<tr>" +
-            "<td>1.</td>" +
-            "<td>" + fave1 + "</td>" +
+            "<td>C# </td>" +
+            "<td>" + progressOptions[cSharpProgress] + "</td>" +
             "</tr>" +
             "<tr>" +
-            "<td>2.</td>" +
-            "<td>" + fave2 + "</td>" +
+            "<td>JavaScript</td>" +
+            "<td>" + progressOptions[jSProgress] + "</td>" +
             "</tr>" +
             "<tr>" +
-            "<td>3.</td>" +
-            "<td>" + fave3 + "</td>" +
+            "<td>Ruby</td>" +
+            "<td>" + progressOptions[rubyProgress] + "</td>" +
             "</tr>" +
             "</table>";
 
